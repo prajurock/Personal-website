dc1f8845729515f79d89e9d8998525ab2fbc0b1b
prajurock <prajwalsingh651@gmail.com>
2021-02-28 02:06:54 +0530

    navigation added

--- src/components/Template/Navigation.js
+++ src/components/Template/Navigation.js
@@ -1,16 +1,29 @@
 import Hamburger from './Hamburger'
+import { Link } from 'react-router-dom';
 import React from 'react'
+import routes from "../../data/routes"
 
-const Navigation = () =>(
+const Navigation = () => (
     <header id="header">
-        <h1>Header</h1>
-    <nav className='links'>
-        <ul>
-        <li>first</li>
-        <li>Seconds</li>
-        </ul>
-    </nav>
-    <Hamburger/>
+        <h1 className="index-link">
+            {routes.filter((r) => r.index).map((r) => (
+                <Link key={r.label} to={r.path}>{r.label}</Link>
+            ))}
+        </h1>
+        <nav className='links'>
+            <ul>
+                {routes.filter((l) => !l.index).map((r) => {
+                        return (
+                            <li key={r.label}>
+                                <Link to={r.path}>{r.label}</Link>
+                            </li>
+                        )
+                    })
+                }
+
+            </ul>
+        </nav>
+        <Hamburger />
     </header>
 
 );
