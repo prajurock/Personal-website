1b55dcacef12f11c450cbd8fe037cbf11aed1496
prajurock <prajwalsingh651@gmail.com>
2021-02-28 01:45:48 +0530

    navigation demo

--- src/components/Template/Navigation.js
+++ src/components/Template/Navigation.js
@@ -1,9 +1,18 @@
+import Hamburger from './Hamburger'
 import React from 'react'
 
 const Navigation = () =>(
-    <header>
+    <header id="header">
         <h1>Header</h1>
+    <nav className='links'>
+        <ul>
+        <li>first</li>
+        <li>Seconds</li>
+        </ul>
+    </nav>
+    <Hamburger/>
     </header>
+
 );
 
 export default Navigation;
