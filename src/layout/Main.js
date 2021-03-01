bbef6f0994784b2310f5554a752682325b3a2c76
prajurock <prajwalsingh651@gmail.com>
2021-03-01 12:14:28 +0530

    component added in main page

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -3,13 +3,41 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Navigation from '../components/Template/Navigation';
 import PropTypes from 'prop-types';
 import React from 'react'
+import SideBar from '../components/Template/SideBar'
 
 const Main = (props) => (
     <HelmetProvider>
-    <Navigation/>
-        {props.description}
+        <Helmet titleTemplate="%s | Prajwal kumar Singh" defaultTitle='Prajwal Kumar Singh'>
+            {props.title && <title>{props.title}</title>}
+            <meta name="description" content={props.description} />
+        </Helmet>
+        <div id="wrapper">
+            <Navigation />
+            <div id="main">
+                {props.children}
+            </div>
+            {props.fullPage ? null : <SideBar />}
+        </div>
+
     </HelmetProvider>
 );
+
+Main.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
+    fullPage: PropTypes.bool,
+    title: PropTypes.string,
+    description: PropTypes.string,
+};
+
+Main.defaultProps = {
+    children: null,
+    fullPage: false,
+    title: null,
+    description: " Prajwal's personal website.",
+};
 
 
 export default Main;
