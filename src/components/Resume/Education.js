2a6b666ff0b21a85ca30eb29fbba3b0430c2e78f
prajurock <prajwalsingh651@gmail.com>
2021-03-03 12:46:01 +0530

    education added

---
+++ src/components/Resume/Education.js
@@ -0,0 +1,19 @@
+import Degree from './Education/Degree';
+import React from 'react';
+
+const Education = ({data}) => (
+    <div className="education">
+    <div className="link-to" id="education" />
+    <div className="title">
+      <h3>Education</h3>
+    </div>
+    {data.map((degree)=>(
+        <Degree
+            data={degree}
+            key={degree.school}
+        />
+    ))
+    }
+    </div>
+);
+export default Education;
